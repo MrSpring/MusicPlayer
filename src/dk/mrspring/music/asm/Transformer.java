a9b385292f1200f632cae81074c0c5054c8c8a0b
MrSpring <mrspring@mrspring.dk>
2015-07-11 01:08:36 +0200

    Stuff

--- src/dk/mrspring/music/asm/Transformer.java
+++ src/dk/mrspring/music/asm/Transformer.java
@@ -13,23 +13,16 @@ import net.minecraft.client.gui.GuiMainMenu;
 /**
  * Created by MrSpring on 07-07-2015 for MC Music Player.
  */
-public class Transformer /*implements IClassTransformer*/ extends EventInjectionTransformer
+public class Transformer extends EventInjectionTransformer
 {
-    //    @Override
-//    public byte[] transform(String s, String s1, byte[] bytes)
-//    {
-//        System.out.println("Transforming");
-//        return bytes;
-//    }
     private static Obf c_GuiMainMenu = new ObfTable("net.minecraft.client.gui.GuiMainMenu", "bxq");
     private static Obf m_initGui = new ObfTable("func_73866_w_", "b", "initGui");
     private static Obf m_actionPerformed = new ObfTable("func_146284_a", "a", "actionPerformed");
+    private static Obf f_buttonList = new ObfTable("field_146292_n", "n", "buttonList");
 
     @Override
     protected void addEvents()
     {
-        System.out.println("Adding events");
-
         Event onInitGui = Event.getOrCreate("onInitMainMenu");
         MethodInfo initGuiInfo = new MethodInfo(c_GuiMainMenu, m_initGui, Void.TYPE);
         this.addEvent(onInitGui, initGuiInfo, new MethodHead());
@@ -43,14 +36,12 @@ public class Transformer /*implements IClassTransformer*/ extends EventInjection
 
     public static void initGui(EventInfo<GuiMainMenu> info)
     {
-        System.out.println("init");
         GuiMainMenu menu = info.getSource();
-        GuiMainMenuHandler.onInitMainMenuGui(menu);
+        GuiMainMenuHandler.onInitMainMenuGui(menu, f_buttonList.names);
     }
 
     public static void actionPerformed(EventInfo<GuiMainMenu> info, GuiButton button)
     {
-        System.out.println("actionPerformed");
         GuiMainMenuHandler.onActionPerformed(button);
     }
 }
